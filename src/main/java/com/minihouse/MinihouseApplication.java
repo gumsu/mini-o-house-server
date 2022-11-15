bfb51ad6740605f0fdbbf0dc44d214cc4377d235
gumsu <chavi9@naver.com>
2022-11-15 23:37:07 +0900

    Initial commit

---
+++ src/main/java/com/minihouse/MinihouseApplication.java
@@ -0,0 +1,13 @@
+package com.minihouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinihouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinihouseApplication.class, args);
+	}
+
+}
