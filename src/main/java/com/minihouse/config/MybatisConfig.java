b1b52c57bf025ffe2fb9ec1b6a3c7ec927203b8e
gumsu <chavi9@naver.com>
2022-11-29 00:01:51 +0900

    #3 MybatisConfig 불필요한 코드 제거 및 변수명 명확히 작성

--- src/main/java/com/minihouse/config/MybatisConfig.java
+++ src/main/java/com/minihouse/config/MybatisConfig.java
@@ -5,7 +5,6 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
@@ -14,30 +13,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan(value = "com.minihouse", sqlSessionFactoryRef = "SqlSessionFactory")
+@MapperScan(value = "com.minihouse", sqlSessionFactoryRef = "sqlSessionFactory")
 public class MybatisConfig {
 
     @Value("${spring.datasource.mapper-locations}")
-    String mPath;
+    String mapperLocations;
 
-    @Bean(name = "dataSource")
+    @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
         return DataSourceBuilder.create().build();
     }
 
 
-    @Bean(name = "SqlSessionFactory")
-    public SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource") DataSource dataSource, ApplicationContext applicationContext) throws Exception {
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource, ApplicationContext applicationContext) throws Exception {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(dataSource);
-        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources(mPath));
+        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources(mapperLocations));
         return sqlSessionFactoryBean.getObject();
     }
 
-    @Bean(name = "SessionTemplate")
-    public SqlSessionTemplate sqlSessionTemplate(@Qualifier("SqlSessionFactory") SqlSessionFactory firstSqlSessionFactory) {
-        return new SqlSessionTemplate(firstSqlSessionFactory);
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
     }
 
 }
