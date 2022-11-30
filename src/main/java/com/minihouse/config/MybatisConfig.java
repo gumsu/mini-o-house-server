28daafb4f1785f2ea41589d1dc0b703df4681298
gumsu <49813916+gumsu@users.noreply.github.com>
2022-12-01 00:25:27 +0900

    프로젝트에 필요한 DB 설정 및 테이블 추가 (#4)
    * #3 mybatis, mysql 의존성 추가
    
    * #3 mybatis setting 추가
    
    * #3 sql 로그 의존성 추가
    
    * #3 테이블 파일 추가
    
    * #3 메서드 네이밍 규칙에 맞게 수정
    
    * #3 메서드 네이밍 규칙에 맞게 수정
    
    * #3 MybatisConfig 불필요한 코드 제거 및 변수명 명확히 작성
    
    * #3 schema 파일 삭제

---
+++ src/main/java/com/minihouse/config/MybatisConfig.java
@@ -0,0 +1,42 @@
+package com.minihouse.config;
+
+import javax.sql.DataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan(value = "com.minihouse", sqlSessionFactoryRef = "sqlSessionFactory")
+public class MybatisConfig {
+
+    @Value("${spring.datasource.mapper-locations}")
+    String mapperLocations;
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource, ApplicationContext applicationContext) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources(mapperLocations));
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+}
