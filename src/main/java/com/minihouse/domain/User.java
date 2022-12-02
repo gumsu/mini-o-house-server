3fc7752d9f41de48629d5c0b7b39b9ed0a279769
gumsu <chavi9@naver.com>
2022-12-02 18:35:30 +0900

    #5 feat: User 엔티티 추가

---
+++ src/main/java/com/minihouse/domain/User.java
@@ -0,0 +1,35 @@
+package com.minihouse.domain;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class User {
+
+    private Long id;
+    private String name;
+    private String nickname;
+    private String email;
+    private String password;
+    private String phone;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String role;
+
+    @Builder
+    public User(Long id, String name, String nickname, String email, String password,
+        String phone, LocalDateTime createdAt, LocalDateTime updatedAt, String role) {
+        this.id = id;
+        this.name = name;
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.role = role;
+    }
+}
