7d072b79bb162a030f26b3c4bf766ad972cfe951
gumsu <49813916+gumsu@users.noreply.github.com>
2022-12-04 15:45:47 +0900

    사용자 데이터베이스 스키마 정의 (#6)
    * #5 docs: user schema 추가
    
    * #5 feat: User 엔티티 추가
    
    * #5 feat: UserRepository 의 save 추가
    
    * #5 feat: userMapper 의 save 작성
    - 성공 후 user의 id를 리턴한다.
    
    * #5 chore: user 엔티티에서 role 제거
    
    * #5 refactor: 회원가입 시 user 생성일자를 now() 로 변경
    
    * #5 chore: user 불필요한 코드 제거

---
+++ src/main/java/com/minihouse/domain/User.java
@@ -0,0 +1,31 @@
+package com.minihouse.domain;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class User {
+
+    private Long id;
+    private String name;
+    private String nickname;
+    private String email;
+    private String password;
+    private String phone;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public User(Long id, String name, String nickname, String email, String password,
+        String phone, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
