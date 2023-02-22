ef47ddefb77fe8aed20e9b355b0fbac4eb0f33fa
gumsu <chavi9@naver.com>
2023-02-22 21:50:22 +0900

    #22 feat: CommentService 댓글 작성 추가

---
+++ src/main/java/com/minihouse/service/CommentService.java
@@ -0,0 +1,31 @@
+package com.minihouse.service;
+
+import com.minihouse.domain.Comment;
+import com.minihouse.domain.Post;
+import com.minihouse.repository.CommentRepository;
+import com.minihouse.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+
+    @Transactional
+    public Long create(Comment comment) {
+        validCreateComment(comment);
+        commentRepository.save(comment);
+        return comment.getId();
+    }
+
+    private void validCreateComment(Comment comment) {
+        Post findPostById = postRepository.findById(comment.getPostId());
+        if (findPostById == null) {
+            throw new IllegalArgumentException("존재하지 않는 게시글입니다.");
+        }
+    }
+}
