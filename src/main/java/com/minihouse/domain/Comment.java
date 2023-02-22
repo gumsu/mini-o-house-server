6684a99e986579dc5380ddf6c143af756b0f6c82
gumsu <chavi9@naver.com>
2023-02-22 21:48:15 +0900

    #22 feat: comment entity 추가

---
+++ src/main/java/com/minihouse/domain/Comment.java
@@ -0,0 +1,29 @@
+package com.minihouse.domain;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class Comment {
+
+    private final Long id;
+    private final String content;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+    private final Long postId;
+    private final Long userId;
+    private final Long commentId;
+
+    @Builder
+    public Comment(Long id, String content, LocalDateTime createdAt, LocalDateTime updatedAt,
+        Long postId, Long userId, Long commentId) {
+        this.id = id;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.postId = postId;
+        this.userId = userId;
+        this.commentId = commentId;
+    }
+}
