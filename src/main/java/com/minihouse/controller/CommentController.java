9807c2f1d2e7ef7b036b0e7527301cd96e0a62b1
gumsu <chavi9@naver.com>
2023-02-22 21:51:37 +0900

    #22 feat: CommentController 생성

---
+++ src/main/java/com/minihouse/controller/CommentController.java
@@ -0,0 +1,23 @@
+package com.minihouse.controller;
+
+import com.minihouse.request.CommentCreateRequest;
+import com.minihouse.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
+@RestController
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/{postId}")
+    public Long registerComment(@PathVariable Long postId, @RequestBody CommentCreateRequest request) {
+        return commentService.create(request.toEntity(postId));
+    }
+}
