9ccbc03dc763aced2df96e69303fc6456dfb0e7b
gumsu <chavi9@naver.com>
2023-02-23 17:16:00 +0900

    #24 feat: BaseException 추상 클래스 작성

---
+++ src/main/java/com/minihouse/exception/BaseException.java
@@ -0,0 +1,21 @@
+package com.minihouse.exception;
+
+import lombok.Getter;
+
+@Getter
+public abstract class BaseException extends RuntimeException {
+
+    public BaseException() {
+        super();
+    }
+
+    public BaseException(String message) {
+        super(message);
+    }
+
+    public BaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public abstract int getStatusCode();
+}
