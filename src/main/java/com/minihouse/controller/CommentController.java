8ca40b66aa8f1f7dca5f600d9f92e866b35dad06
gumsu <chavi9@naver.com>
2023-02-23 22:08:23 +0900

    #22 feat: CommentController update 추가

--- src/main/java/com/minihouse/controller/CommentController.java
+++ src/main/java/com/minihouse/controller/CommentController.java
@@ -1,8 +1,11 @@
 package com.minihouse.controller;
 
 import com.minihouse.request.CommentCreateRequest;
+import com.minihouse.request.CommentUpdateRequest;
 import com.minihouse.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,5 +22,12 @@ public class CommentController {
     @PostMapping("/{postId}/comments")
     public Long registerComment(@PathVariable Long postId, @RequestBody CommentCreateRequest request) {
         return commentService.create(request.toEntity(postId));
+    }
+
+    @PostMapping("/{postId}/comments/{commentId}")
+    public ResponseEntity<HttpStatus> updateComment(@PathVariable("postId") Long postId, @PathVariable("commentId") Long commentId, @RequestBody CommentUpdateRequest request) {
+        String content = request.getContent();
+        commentService.update(postId, commentId, content);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
