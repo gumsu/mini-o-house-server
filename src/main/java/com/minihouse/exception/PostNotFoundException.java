a2f0f1b7517664afdb759760a68cafa0fab1c913
gumsu <chavi9@naver.com>
2023-02-23 16:48:51 +0900

    #24 feat: PostNotFoundException 작성

---
+++ src/main/java/com/minihouse/exception/PostNotFoundException.java
@@ -0,0 +1,16 @@
+package com.minihouse.exception;
+
+public class PostNotFoundException extends RuntimeException {
+
+    public PostNotFoundException() {
+        super();
+    }
+
+    public PostNotFoundException(String message) {
+        super(message);
+    }
+
+    public PostNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
