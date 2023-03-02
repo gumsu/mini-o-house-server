49e7c5589305091c0cd7c65fbb536d3a0f65a3f3
gumsu <chavi9@naver.com>
2023-03-02 23:35:36 +0900

    #27 feat: 로그인 인가를 위한 argument resolver 구현

---
+++ src/main/java/com/minihouse/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.minihouse.config;
+
+import com.minihouse.resolver.AuthUserArgumentResolver;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthUserArgumentResolver authUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authUserArgumentResolver);
+    }
+}
