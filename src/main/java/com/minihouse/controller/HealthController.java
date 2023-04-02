41b100d64cfe8226fb4120253ffba22ca7215c14
gumsu <chavi9@naver.com>
2023-04-02 23:22:19 +0900

    #18 health check controller 추가하여 서버 정상 작동 확인

---
+++ src/main/java/com/minihouse/controller/HealthController.java
@@ -0,0 +1,17 @@
+package com.minihouse.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<HttpStatus> healthCheck() {
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
