da424378dc5b78aa9717c872b7bfb02c9c970c76
gumsu <chavi9@naver.com>
2023-04-03 23:18:56 +0900

    #8 interceptor 로 인해 REST docs 를 호출할 수 없었던 문제 수정

--- src/main/java/com/minihouse/config/WebConfig.java
+++ src/main/java/com/minihouse/config/WebConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
@@ -23,6 +24,14 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(signInInterceptor);
+        registry.addInterceptor(signInInterceptor)
+            .addPathPatterns("/**")
+            .excludePathPatterns("/docs/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/docs/**")
+            .addResourceLocations("classpath:/static/docs/");
     }
 }
